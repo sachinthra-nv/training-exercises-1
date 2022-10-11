2b79d4f3de79c4d3f537f27dbdb63dd0dec5fc30
sachinthra-nv <sachinthra-n-v@hpe.com>
2022-10-11 16:15:22 +0530

    updated

--- functions/functions.py
+++ functions/functions.py
@@ -5,6 +5,8 @@ def forEachDict(dc):
     findImageTag(dc, keys)
 
 def findImageTag(dc,keys):
+    if 'image' in keys:
+        print("woooooow: ",dc['image'])
     for i in keys:
         # print(i)
         # print(type(dc[i]) is list or type(dc[i]) is dict)
@@ -12,15 +14,11 @@ def findImageTag(dc,keys):
             # print(dc[i])
             newKeys = list(dc[i].keys())
             # print(newKeys)
-            if 'image' in newKeys:
-                print("woooooow: ",dc[i]['image'])
             findImageTag(dc[i],newKeys)
         elif type(dc[i]) is list:
             for j in dc[i]:
                 if type(j) is not str:
                     newKeys = list(j.keys())
-                    if 'image' in newKeys:
-                        print("wooooooow: ",j['image'])
                     findImageTag(j,newKeys)
         
     return
