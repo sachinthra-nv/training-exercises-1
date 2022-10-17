1b4ae312ba60dacae64a1ef41dfd42914057bc7c
sachinthra-nv <sachinthra-n-v@hpe.com>
2022-10-17 16:08:37 +0530

    batch functions

--- tests/test.sh
+++ tests/test.sh
@@ -3,26 +3,37 @@
 # cat src/data/final.yaml | grep imagePullPolicy: | wc -l
 # cat src/data/capk.yaml | grep imagePullPolicy: | wc -l
 
-echo UT1 - Number of image Tag
-echo UT2 - Number of imagePullPolicy Tag
+ut1
 echo \\n
+ut2
 
-ORGINAL_IMAGE_TAG=`cat src/data/final.yaml | grep image: | wc -l`
-FINAL_IMAGE_TAG=`cat src/data/capk.yaml | grep image: | wc -l`
+function ut1() {
 
-if [ $ORGINAL_IMAGE_TAG == $FINAL_IMAGE_TAG ]; then
-    echo "UT1 Passed"
-else
-    echo "UT1 Failed"
-fi
+    echo UT1 - Number of image Tag
 
-ORGINAL_IPP_TAG=`cat src/data/final.yaml | grep imagePullPolicy: | wc -l`
-FINAL_IPP_TAG=`cat src/data/capk.yaml | grep imagePullPolicy: | wc -l`
-CHANGED=$(expr $ORGINAL_IPP_TAG - $FINAL_IPP_TAG)
+    ORGINAL_IMAGE_TAG=`cat src/data/final.yaml | grep image: | wc -l`
+    FINAL_IMAGE_TAG=`cat src/data/capk.yaml | grep image: | wc -l`
 
-if [ $CHANGED != 0 ]; then
-    echo "UT2 - Changed/Added $CHANGED imagePullPolicy Tag"
-else
-    echo "UT2 - No additional imagePullPolicy Tag"
-fi
+    if [ $ORGINAL_IMAGE_TAG == $FINAL_IMAGE_TAG ]; then
+        echo "UT1 Passed"
+    else
+        echo "UT1 Failed"
+    fi
 
+}
+
+function ut2() {
+
+    echo UT2 - Number of imagePullPolicy Tag
+
+    ORGINAL_IPP_TAG=`cat src/data/final.yaml | grep imagePullPolicy: | wc -l`
+    FINAL_IPP_TAG=`cat src/data/capk.yaml | grep imagePullPolicy: | wc -l`
+    CHANGED=$(expr $ORGINAL_IPP_TAG - $FINAL_IPP_TAG)
+
+    if [ $CHANGED != 0 ]; then
+        echo "UT2 - Changed/Added $CHANGED imagePullPolicy Tag"
+    else
+        echo "UT2 - No additional imagePullPolicy Tag"
+    fi
+
+}
